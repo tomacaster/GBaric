6f7bf0eea1252614d09495034c8909de484453a9
Michał Tomaszewski <michal.tomaszewski@nanovo.pl>
2024-03-23 03:58:29 +0200

    cmake fixes; gtk builder initial commit

--- src/ApplicationBase.cpp
+++ src/ApplicationBase.cpp
@@ -1,9 +1,64 @@
 #include "ApplicationBase.h"
 #include "MainWindow.h"
+#include <iostream>
+#include <chrono>
+#include <thread>
+#ifdef __linux__
+#include <gdk/gdkx.h>
+#else
+#include <gdk/win32/gdkwin32.h>
+#endif
+#include <gtk/gtk.h>
 
 ApplicationBase::ApplicationBase(std::string appName) : Gtk::Application(appName)
 {
+auto refBuilder = Gtk::Builder::create();
+    try
+    {
+#ifdef __linux__
+        auto res = refBuilder->add_from_resource("/resources/gui.glade");
+        if(!res)
+        {
+            std::cout << "Cannot load resources" << endl;
 
+            return false;
+        }  
+#else
+        auto res = refBuilder->add_from_file("resources\\gui.glade");
+        if(!res)
+        {
+            std::cout << "Cannot load resources" << endl;
+
+            return false;
+        } 
+#endif    
+    }
+    catch(const Glib::FileError& ex)
+    {
+        std::cerr << "FileError: " << ex.what() << endl;
+        return false;
+    }
+    catch(const Glib::MarkupError& ex)
+    {
+        cerr << "MarkupError: " << ex.what() << endl;
+        return false;
+    }
+    catch(const Gtk::BuilderError& ex)
+    {
+        cerr << "BuilderError: " << ex.what() << endl;
+        return false;
+    }
+
+    refBuilder->get_widget_derived("MainWindow", windowMain);
+    if(windowMain)
+    {
+        windowMain->signal_delete_event().connect(sigc::mem_fun(*this, &AVRenderer::OnDestroy));    
+        windowMain->playButton->signal_clicked().connect( sigc::mem_fun(*this,&AVRenderer::OnPlayButtonPressed) ); 
+        windowMain->pauseButton->signal_clicked().connect( sigc::mem_fun(*this,&AVRenderer::OnPauseButtonPressed) ); 
+        windowMain->sliderData->sliderData->signal_button_press_event().connect(sigc::mem_fun(*this,&AVRenderer::OnSliderButtonPressed));
+        windowMain->sliderData->sliderData->signal_button_release_event().connect(sigc::mem_fun(*this,&AVRenderer::OnSliderReleased));
+        windowMain->renderSurface->signal_realize().connect(sigc::mem_fun(*this, &AVRenderer::OnCreateMain));
+    }
 }
 
 Glib::RefPtr<ApplicationBase> ApplicationBase::create(std::string appName)
